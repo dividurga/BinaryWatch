0b5c9cbc40666bbeb6520952846bce32a54a0359
Divija Durga <durgadivija@gmail.com>
2024-07-30 22:42:11 +0530

    Update watchFirmwareSTM32.ino

--- watch_STM32/watchFirmwareSTM32.ino
+++ watch_STM32/watchFirmwareSTM32.ino
@@ -13,26 +13,32 @@ where 1 = LED ON and 0 = LED OFF
 This code makes optimizations, like using the lowpower library to conserve battery life, and using the EEPROM memory
 to store if the time has been reset already.
 
-Created by Divija Durga, 23 July, 2024
+Created by Divija Durga, 29 July, 2024
 */
 
 #include <STM32RTC.h>
 #include "STM32LowPower.h"
-#include <EEPROM.h> // Include EEPROM library
-
+#include <EEPROM.h>  // Include EEPROM library
+bool awake = false;
+bool awakened = false;
+uint8_t hour;
+uint8_t minutes;
+int onTime=3000;
+unsigned long sleepTimer;
 // Get the rtc object
 STM32RTC& rtc = STM32RTC::getInstance();
-
+uint8_t matrix[4][4];
 // Define pin constants
-const int pin = PA0; // Pin for waking up
-const uint8_t col[4] = {PB2, PB1, PB0, PA7}; // Columns of the LED matrix
-const uint8_t row[4] = {PA8, PA9, PC6, PA10}; // Rows of the LED matrix
+const int pin = PA6;                             // Pin for waking up
+const uint8_t col[4] = { PB2, PB1, PB0, PA7 };   // Columns of the LED matrix
+const uint8_t row[4] = { PA8, PA9, PC6, PA10 };  // Rows of the LED matrix
 
-const int eepromAddress = 0; // EEPROM address to store the reset flag
+const int eepromAddress = 0;  // EEPROM address to store the reset flag
+volatile unsigned long prevInterrupt = 0;   //used for debouncing push button
 
 void setup() {
-  Serial.begin(9600); // Start serial communication at 9600 baud rate
-// Initialize the column and row pins
+  Serial.begin(9600);  // Start serial communication at 9600 baud rate
+                       // Initialize the column and row pins
   for (int i = 0; i < 4; i++) {
     pinMode(col[i], OUTPUT);
     pinMode(row[i], OUTPUT);
@@ -42,12 +48,12 @@ void setup() {
 
   // Configure low power mode
   LowPower.begin();
-  
+
   // Select RTC clock source: LSI_CLOCK, LSE_CLOCK or HSE_CLOCK.
   // By default the LSI is selected as source.
   //rtc.setClockSource(STM32RTC::LSE_CLOCK);
 
-  rtc.begin(); // Initialize RTC in 24H format
+  rtc.begin();  // Initialize RTC in 24H format
   // Check if the RTC time has been set before
   if (EEPROM.read(eepromAddress) != 1) {
     // RTC time has not been set, set it to compile time
@@ -77,44 +83,64 @@ void setup() {
 }
 
 void loop() {
-  // Empty loop
-}
+  if (awake) {
+    if (millis() - prevInterrupt > 100)  //if button has been pressed for 100ms
+    {
+      if (!digitalRead(pin)) {
+        awakened = true;        //flag to start led matrix
+        sleepTimer = millis();  //start the sleep countdown
+        awake = false;
+        // Display hours and minutes in binary on LED matrix
+        hour = rtc.getHours();
+        minutes = rtc.getMinutes();
+      } else
+        awake = false;
+    }
+  }
 
-// Print date and time
-void printTime() {
-  // Print time
-  Serial.printf("%02d:%02d:%02d.%03d\n", rtc.getHours(), rtc.getMinutes(), rtc.getSeconds(), rtc.getSubSeconds());
-
-  // Display hours and minutes in binary on LED matrix
-  uint8_t hour = rtc.getHours();
-  uint8_t min = rtc.getMinutes();
-
+  if (awakened) {
+    if(millis() - sleepTimer < onTime){
   // Set LED for hour tens position
   binaryColumn(0, hour / 10);
   // Set LED for hour ones position
   binaryColumn(1, hour % 10);
   // Set LED for minutes tens position
-  binaryColumn(2, min / 10);
+  binaryColumn(2, minutes / 10);
   // Set LED for minutes ones position
-  binaryColumn(3, min % 10);
+  binaryColumn(3, minutes % 10);
 
-  // Display for two seconds
-  delay(2000);
-  clearMatrix();
+    }
+    else {
+      awake=false;
+      awakened=false;
+    }
+  }
+
+  if (!awake && ! awakened) {
+LowPower.sleep();
+  }
+}
+
+// Print date and time
+void printTime() {
+  awake = true;
+  prevInterrupt = millis();
 }
 
 // Set a specific LED in the matrix
 void setLED(uint8_t rowNum, uint8_t colNum) {
-  digitalWrite(col[colNum], HIGH); // Set column HIGH
-  digitalWrite(row[rowNum], LOW);  // Set row LOW
+  digitalWrite(col[colNum], HIGH);  // Set column HIGH
+  digitalWrite(row[rowNum], LOW);   // Set row LOW
 }
 
 // Clear the LED matrix
 void clearMatrix() {
   // Set all columns to LOW and all rows to HIGH to clear the matrix
   for (int i = 0; i < 4; i++) {
-    digitalWrite(col[i], LOW);
-    digitalWrite(row[i], HIGH);
+    for (int j = 0; j < 4; j++) {
+      digitalWrite(col[i], LOW);
+      digitalWrite(row[j], HIGH);
+    }
   }
 }
 
@@ -132,11 +158,11 @@ void binaryColumn(uint8_t colPin, uint8_t num) {
 
   // Set LEDs based on the binary representation of the number
   for (uint8_t i = 0; i < numLEDs; i++) {
-    uint8_t bit = (num >> i) & 1; // Get the bit at position i
+    uint8_t bit = (num >> i) & 1;  // Get the bit at position i
     if (bit == 1) {
-      setLED(i, colPin); // Turn on the LED if the bit is 1
-      // For debugging purposes
-      Serial.println("(" + String(i) + ", " + String(colPin) + ")");
+      setLED(i, colPin);  // Turn on the LED if the bit is 1
+      delayMicroseconds(300);
+      clearMatrix();
     }
   }
 }
